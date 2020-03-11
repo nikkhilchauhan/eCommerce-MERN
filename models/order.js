82879e77c979dbbf8cdd9ba76458026e2391a0b6
Nikhil Chauhan <nikkhil0018@Nikhils-MacBook-Pro.local>
2020-03-11 14:57:38 +0530

    Done with order Schema

---
+++ models/order.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Schema;
+
+const productCartSchema = new mongoose.Schema({
+  product: {
+    type: ObjectId,
+    ref: 'Product'
+  },
+  name: String,
+  count: Number,
+  price: Number
+});
+const ProductCart = mongoose.model('Order', productCartSchema);
+
+const orderSchema = new mongoose.Schema(
+  {
+    products: [productCartSchema],
+    transaction_id: {},
+    amount: { type: Number },
+    address: String,
+    updated: Date,
+    user: {
+      type: ObjectId,
+      ref: 'User'
+    }
+  },
+  { timestamps: true }
+);
+const Order = mongoose.model('Order', orderSchema);
+module.exports = { Order, ProductCart };
