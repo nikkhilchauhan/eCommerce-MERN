4b9561432fe8a5612330afa42382595e9c015f21
Nikhil Chauhan <nikkhil0018@Nikhils-MacBook-Pro.local>
2020-03-16 19:39:33 +0530

    Done with connecting to MongoDB

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
+// Connecting to MongoDB Database
+mongoose
+  .connect('mongodb://localhost:27017/tshirt', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .then(() => {
+    console.log('MongoDB is Connected');
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+// Configuration for Nodejs
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`App is running at http://localhost:${PORT}/`);
+});
 app.get('/', (req, res) => {
   res.send('You are genius');
-});
-
-app.listen(8000, () => {
-  console.log('Running');
 });
