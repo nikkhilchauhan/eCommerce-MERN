d0f524c4a6024c95ffae6c19307f1081fbf8782a
nikkhilchauhan <nikhilchauhan0018@gmail.com>
2020-04-20 23:27:07 -0700

    client - createOrder helper

---
+++ client/src/core/helper/OrderHelper.js
@@ -0,0 +1,15 @@
+export const createOrder = (userId, authToekn, orderData) => {
+  return fetch(`/api/order/create/${userId}`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${authToekn}`,
+    },
+    body: JSON.stringify({ order: orderData }),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
